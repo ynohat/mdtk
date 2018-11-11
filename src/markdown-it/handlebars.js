5a632647ac3fb5f63aadb314b1ee45b749fffcf6
Anthony Hogg <ahogg@akamai.com>
2018-11-11 11:18:42 +0100

    remove handlebars `resolve` helper

--- src/markdown-it/handlebars.js
+++ src/markdown-it/handlebars.js
@@ -9,13 +9,6 @@ module.exports = function handlebars(md, options) {
     debug("init");
 
     Handlebars.registerHelper("json", JSON.stringify);
-    Handlebars.registerHelper("resolve", function (relPath) {
-        debug("resolve", relPath);
-        const path = require("path");
-        var {resolve} = require("../utils");
-        var absPath = resolve(relPath, ...options.include);
-        return require("path").relative(path.dirname(options.output), absPath);
-    });
 
     md.core.ruler.before("normalize", "mdtk-handlebars", function mdtk_handlebars(state) {
         state.src = Handlebars.compile(state.src).call(null, options && options.vars || {});
